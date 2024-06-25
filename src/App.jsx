862ec87600c6e2e1a872019f833867b4767402f6
JeffersonColmenares-PdP <jefferson.colmenares@cumplimos.co>
2024-06-25 02:36:10 -0500

    creacion estructura para navegar

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom"
 import './App.css'
+import Principal from './components/Principal'
+import Paises from "./components/Paises";
+import RickAndMorty from "./components/RickAndMorty";
+import TablaPaises from "./components/TablaPaises";
+import TablaTraduccionPaises from "./components/TablaTraduccionPaises";
+import TablaFronteras from "./components/TablaFronteras";
+import TablaEspecies from "./components/TablaEspecies";
+import TablaPersonajes from "./components/TablaPersonajes";
+import TablaPaisPersonaje from "./components/TablaPaisPersonaje";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const navegar = useNavigate();
+
+  useEffect(() => {
+    navegar('/');
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" element={<Principal />} />
+        <Route path="/paises" element={<Paises />} />
+        <Route path="/tabla-paises" element={<TablaPaises />} />
+        <Route path="/tabla-traduccion-paises" element={<TablaTraduccionPaises />} />
+        <Route path="/tabla-fronteras" element={<TablaFronteras />} />
+        <Route path="/rick-and-morty" element={<RickAndMorty />} />
+        <Route path="/tabla-personajes" element={<TablaPersonajes />} />
+        <Route path="/tabla-especies" element={<TablaEspecies />} />
+        <Route path="/tabla-pais-personaje" element={<TablaPaisPersonaje />} />
+      </Routes>
     </>
   )
 }
